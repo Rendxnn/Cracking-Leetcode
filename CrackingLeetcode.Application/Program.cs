f691c5243da351997e199674ebfe030c84f9c5ec
samuel rendon <samiretru@gmail.com>
2025-02-10 02:04:16 -0500

    38(count and say): projects rename

---
+++ CrackingLeetcode.Application/Program.cs
@@ -0,0 +1,16 @@
+﻿using CrackingLeetcode.lib;
+
+namespace CrackingLeetcode.app;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(MediumCountAndSay.CountAndSay(1));
+        Console.WriteLine(MediumCountAndSay.CountAndSay(2));
+        Console.WriteLine(MediumCountAndSay.CountAndSay(3));
+        Console.WriteLine(MediumCountAndSay.CountAndSay(4));
+        Console.WriteLine(MediumCountAndSay.CountAndSay(5));
+        Console.WriteLine(MediumCountAndSay.CountAndSay(6));
+    }
+}
